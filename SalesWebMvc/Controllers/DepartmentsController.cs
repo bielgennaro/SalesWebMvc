6e692dcb2f7eec891ab3c0ca1ca469447c7aa1e6
Gabriel Zirondi Di Gennaro <zirondigabriel@gmail.com>
2024-01-25 00:38:14 -0300

    Criando view e controller de Depaertamentos

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var departmentsList = new List<Department>()
+            {
+                new Department
+                {
+                    Id = 1,
+                    Name = "Eletrônicos",
+                    DepartmentCode = 1823
+                },
+
+                new Department
+                {
+                    Id = 2,
+                    Name = "Fashion",
+                    DepartmentCode = 1845
+                }
+            };
+
+            return View(departmentsList);
+        }
+    }
+}
--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.Viewmodels;
 
 using System.Diagnostics;
 
